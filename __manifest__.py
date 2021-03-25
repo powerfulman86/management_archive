fd3e8cea9e8acd23afdc2a4b3b7f6f9a53627ef3
powerfulman86 <dba.ahmedmaher@gmail.com>
2021-03-25 10:04:06 +0200

    enhance

--- __manifest__.py
+++ __manifest__.py
@@ -28,6 +28,7 @@
         'security/ir.model.access.csv',
         'data/sequence.xml',
         'views/menu.xml',
+        'views/management_archive_view.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
